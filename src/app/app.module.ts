18a90bc5a7d07993c99a1eb7d7d045bd57c2b63c
xpec-sherrera <shrrera@formax.cl>
2018-04-30 15:57:12 -0500

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+{ path: '', component: HomeComponent } 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
